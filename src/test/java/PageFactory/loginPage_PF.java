80ad5c912e3b37e47cecaf3aaab41f8252f1ddcc
Ajaaj Ahmad <ajaaj75@gmail.com>
2025-05-06 00:25:04 +0530

    Added Page Factory Model

--- src/test/java/PageFactory/loginPage_PF.java
+++ src/test/java/PageFactory/loginPage_PF.java
@@ -1,20 +1,35 @@
 package PageFactory;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class loginPage_PF {
 
 	@FindBy(name = "username")
 	WebElement txt_username;
+
 	@FindBy(name = "password")
 	WebElement txt_password;
-	@FindBy(className = "submit")
+
+	@FindBy(id = "submit")
 	WebElement btn_login;
-	
-	public void loginWithValidCred() {
+
+	@FindBy(className = "post-title")
+	WebElement post_title;
+
+	WebDriver driver;
+
+	public loginPage_PF(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver,loginPage_PF.class);
+	}
+
+	public void loginWithValidCred(String username,String password) {
 		txt_username.sendKeys();
 		txt_password.sendKeys();
-		
+		btn_login.click();
+		post_title.isDisplayed();
 	}
 }
