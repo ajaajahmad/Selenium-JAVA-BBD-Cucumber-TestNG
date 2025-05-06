2f7ee12fbf33f77edcd5bc6ad17551142e5990a5
Ajaaj Ahmad <ajaaj75@gmail.com>
2025-05-06 23:03:14 +0530

    Added Page Factory Model

--- src/test/java/PageFactory/loginPage_PF.java
+++ src/test/java/PageFactory/loginPage_PF.java
@@ -1,26 +1,44 @@
 package PageFactory;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
 public class LoginPage_PF {
 
 	@FindBy(name = "username")
+	@CacheLookup
 	WebElement txt_username;
 
 	@FindBy(name = "password")
+	@CacheLookup
 	WebElement txt_password;
 
 	@FindBy(id = "submit")
+	@CacheLookup
 	WebElement btn_login;
+
+	// We can get link texts as well in
+	@FindBy(partialLinkText = "ajaaj")
+	List<WebElement> myLinks;
+
+	// Another way of getting elements
+	@FindBy(how = How.ID, using = "name")
+	WebElement test;
 
 	WebDriver driver;
 
 	public LoginPage_PF(WebDriver driver) {
 		this.driver = driver;
-		PageFactory.initElements(driver,this);
+		// AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(driver,
+		// 30);
+		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 30), this);
 	}
 
 	public void enterUsername(String username) {
